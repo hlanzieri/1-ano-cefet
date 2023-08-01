b63a84e2ca587e9b0ec51d383684a59c5fe98975
hlanzieri <134455113+hlanzieri@users.noreply.github.com>
2023-08-01 15:59:59 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,72 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+float nota[5], nota2[5], media[5], maiormedia=0;
+int i, imaior;
+char nome[30][5];
 
-int main()
+void main()
 {
-    int i, codigo[40], estoque[40], resp=0, resp2=0, resp3=0, resp4=0;
-    float preco[40];
-    char nome[15][40];
-    printf("*******************CADASTRO*************************\n");
-    for(i=0; i<40; i++)
+    for(i=0; i<5; i++)
     {
-        printf("\nInsira o nome do produto:\n");
-        __fpurge(stdin);
-        gets(nome[i]);
-        printf("\nInsira o código do produto:\n");
-        scanf("%i", &codigo[i]);
-        printf("\nInsira a quantidade em estoque do produto:\n");
-        scanf("%i", &estoque[i]);
-        printf("\nInsira o preço de venda do produto:\n");
-        scanf("%f", &preco[i]);
+    printf("Insira a nota 1:\n");
+    scanf("%f", &nota[i]);
+    printf("\nInsira a nota 2:\n");
+    scanf("%f", &nota2[i]);
+    printf("\n Insira seu nome:\n");
+    __fpurge(stdin);
+    gets(nome[i]);
+    media[i]=(nota[i]+nota2[i])/2;
+    if(media[i]>=6)
+    {
+        printf("\nAprovado");
     }
-    do
+    else
     {
-        printf("\n\n===============FARMÁCIA DO CEFET==================\n");
-        printf("\n\n1. Mostrar preço de venda de um determinado produto;\n2. Efetuar uma venda;\n3. Mostrar todos os dados de todos os programas;\n4. Sair.\nInsira a opção desejada:\n");
-        scanf("%i", &resp);
-        switch(resp)
+        printf("\nReprovado");
+    }
+    if(media[i]>maiormedia)
+    {
+        maiormedia=media[i];
+        imaior=i;
+    }
+    }
+    printf("\n\n A  meior média foi de %.2f pontos", maiormedia);
+    printf("A primeira nota do segundo aluno, o/a %s, foi de %.2f pontos", nome[1], nota[1]);
+    for(i=0; i<5; i++)
+    {
+        printf("\nO/A aluno/a %s tirou %.2f pontos na primeira prova, %.2f pontos na segunda prova e ficou com %.2f pontos de média %.2f", nome[i], nota[i], nota2[i], media[i]);
+    }
+    printf("\nO/A aluno %s foi o que obteve maior média nas duas provas");
+    for(i=0; i<5; i++)
+    {
+        if(strcmp(nome[i],"Tati")==0)
         {
-            case 1:
-            printf("\nInsira o código do produto que você gostaria de ver:\n");
-            scanf("%i", &resp2);
-            for(i=0; i<40; i++)
-            {
-                if(resp2==codigo[i])
-                {
-                    printf("\n\nO produto %s custa R$%.2f", nome[i], preco[i]);
-                }
-            }
-            break;
-            case 2:
-            printf("\nInsira o código do produto que será vendido:\n");
-            scanf("%i", &resp3);
-            printf("\nInsira a quantidade que será demandada:\n");
-            scanf("%i", &resp4);
-            for(i=0;i<40; i++)
-            {
-                if(resp3==codigo[i])
-                {
-                    if(estoque[i]<resp4)
-                    {
-                        printf("\nQUANTIDADE INSUFICIENTE, VENDA CANCELADA.\n");
-                    }
-                    else
-                    {
-                        estoque[i]=estoque[i]-resp4;
-                        printf("\nA quantidade em estoque do produto %s, após a venda é de %i unidades\n", nome[i], estoque[i]);
-                    }
-                }
-            }
-            break;
-            case 3:
-            printf("\n\nA seguir, temos as informações de todos os produtos:\n");
-            for(i=0; i<40; i++)
-            {
-                printf("\nO produto %s, de código %i custa R$%.2f e temos %i unidades em estoque.", nome[i], codigo[i], preco[i], estoque[i]);
-            }
-            break;
+            printf("\nO primeiro aluno se chama Tati");
+        }
+        else
+        {
+            printf("\nO primeiro aluno não se chama Tati");
         }
     }
-    while(resp!=4);
-    printf("\nSaindo...");
     return 0;
 }

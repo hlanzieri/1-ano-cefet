e5a0c67275056b7314dde559c90dfbf14d874144
hlanzieri <134455113+hlanzieri@users.noreply.github.com>
2023-08-03 13:54:19 -0300

    main.c (14)

--- main.c
+++ main.c
@@ -1,54 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-float nota[5], nota2[5], media[5], maiormedia=0;
-int i, imaior;
-char nome[5][30];
-
-void main()
+int main()
 {
-    for(i=0; i<5; i++)
+    int idade[10], i;
+    for(i=0; i<10; i++)
     {
-    printf("Insira a nota 1:\n");
-    scanf("%f", &nota[i]);
-    printf("\nInsira a nota 2:\n");
-    scanf("%f", &nota2[i]);
-    printf("\n Insira seu nome:\n");
-    __fpurge(stdin);
-    gets(nome[i]);
-    media[i]=(nota[i]+nota2[i])/2;
-    if(media[i]>=6)
-    {
-        printf("\nAprovado");
+        printf("\nInsira sua idade:\n");
+        scanf("%i", &idade[i]);
     }
-    else
+    for(i=9; i>=0; i--)
     {
-        printf("\nReprovado");
+        printf("\nIdade %i", idade[i]);
     }
-    if(media[i]>maiormedia)
+    printf("\nA quarta pessoa tem %i anos", idade[3]);
+    idade[6]=18;
+    idade[1]=idade[0];
+    idade[4]=idade[0]*idade[4];
+    printf("\nA seguir a idade de todos:");
+    for(i=0; i<10; i++)
     {
-        maiormedia=media[i];
-        imaior=i;
-    }
-    }
-    printf("\n\n A  meior média foi de %.2f pontos", maiormedia);
-    printf("A primeira nota do segundo aluno, o/a %s, foi de %.2f pontos", nome[1], nota[1]);
-    for(i=0; i<5; i++)
-    {
-        printf("\nO/A aluno/a %s tirou %.2f pontos na primeira prova, %.2f pontos na segunda prova e ficou com %.2f pontos de média %.2f", nome[i], nota[i], nota2[i], media[i]);
-    }
-    printf("\nO/A aluno %s foi o que obteve maior média nas duas provas");
-    for(i=0; i<5; i++)
-    {
-        if(strcmp(nome[i],"Tati")==0)
-        {
-            printf("\nO primeiro aluno se chama Tati");
-        }
-        else
-        {
-            printf("\nO primeiro aluno não se chama Tati");
-        }
+        printf("\n%i", idade[i]);
     }
     return 0;
 }
